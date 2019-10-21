7cd0ba904c6f17388e3ed1580a45953a93a8bb3d
Niklas1997Git <56765399+Niklas1997Git@users.noreply.github.com>
2019-10-21 16:02:49 +0200

    Teamprojekt Empty Acticity

--- Teamprojekt/app/src/androidTest/java/com/example/teamprojekt/ExampleInstrumentedTest.java
+++ Teamprojekt/app/src/androidTest/java/com/example/teamprojekt/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.teamprojekt11;
+package com.example.teamprojekt;
 
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.teamprojekt11", appContext.getPackageName());
+        assertEquals("com.example.teamprojekt", appContext.getPackageName());
     }
 }
--- Teamprojekt/app/src/test/java/com/example/teamprojekt/ExampleUnitTest.java
+++ Teamprojekt/app/src/test/java/com/example/teamprojekt/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.teamprojekt11;
+package com.example.teamprojekt;
 
 import org.junit.Test;
 
