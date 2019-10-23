2e806c00619692c24031dc9eaf7264155db9839c
Niklas1997Git <56765399+Niklas1997Git@users.noreply.github.com>
2019-10-23 14:39:45 +0200

    Erstellen der Fenster und deren Klassen

---
+++ Teamprojekt/app/src/main/java/com/example/teamprojekt/FerngesteuerterModusActivity.java
@@ -0,0 +1,14 @@
+package com.example.teamprojekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FerngesteuerterModusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ferngesteuerter_modus);
+    }
+}
